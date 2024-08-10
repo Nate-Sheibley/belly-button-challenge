de3907eb719a4902006d5f29c8928088291c6799
Nate Sheibley <n.sheibley@gmail.com>
2024-08-09 17:56:52 -0700

    cleaned up metadata for loop to forEach

--- static/js/app.js
+++ static/js/app.js
@@ -2,10 +2,11 @@
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
+    console.log(`buildMetadata called for: ${sample}`);
     // get the metadata field
     let metadata = data.metadata;
     // Filter the metadata for the object with the desired sample number
-    let filtered  = metadata.filter(sampleData => sampleData.id === parseInt(sample));
+    let filtered = metadata.filter(sampleData => sampleData.id === parseInt(sample));
     
     // Use d3 to select the panel with id of `#sample-metadata`
     let metadata_panel = d3.select('#sample-metadata');
@@ -15,14 +16,14 @@ function buildMetadata(sample) {
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-    let keys = Object.keys(filtered[0]);
-    let values = Object.values(filtered[0]);
-    for (let i = 0; i < keys.length; i++) {
-      // generate metadata string to display
-      let line_string = `${keys[i].toUpperCase()}: ${values[i]}`
-      // display metadata as tags in metadata div
+    let meta_data_object = filtered[0];
+    let keys = Object.keys(meta_data_object);
+
+    keys.forEach(key => {
+      let line_string = `${key.toUpperCase()}: ${meta_data_object[key]}`
       metadata_panel.append('p').text(line_string);
-    }
+    });
+    
     console.log(`Metadata updated for sample: ${sample}`);
   });
 }
@@ -56,6 +57,7 @@ function buildCharts(sample) {
       marker:{
         size: sample_values,
         color: otu_ids,
+        //change scale so it's closer to the example
         sizeref: 1.5
       }
     }];
@@ -74,14 +76,19 @@ function buildCharts(sample) {
 
     // Render the Bubble Chart  
     Plotly.newPlot("bubble", bubble_data, bubble_layout);
-
+    console.log(`Bubble plot for ${sample} made`);
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
+    // Build a Bar Chart
+    // Don't forget to slice and reverse the input data appropriately
+  
     let bar_data = [{
       type: 'bar',
       orientation: 'h',
       x: sample_values,
+      //This slices the top 10 AFTER sorting... does not look like it should
       y: otu_ids.map(id => `OTU ${id}`).slice(0,10),
       text: otu_labels,
+      // sort the data ascending according to sample_values
       transforms: [{
         type: 'sort',
         target: 'x',
@@ -96,12 +103,9 @@ function buildCharts(sample) {
       }
     };
 
-    // Build a Bar Chart
-    // Don't forget to slice and reverse the input data appropriately
-
-
     // Render the Bar Chart
     Plotly.newPlot("bar", bar_data, bar_layout);
+    console.log(`Bar chart for ${sample} made`);
   });
 }
 
@@ -124,6 +128,7 @@ function init() {
     // Get the first sample from the list
     let first_sample = dropdown.select('option').property("value");
     // Build charts and metadata panel with the first sample
+    console.log('init() ended. Moving on to first sample')
     optionChanged(first_sample);
   });
 };
@@ -136,7 +141,7 @@ function optionChanged(newSample) {
   buildCharts(newSample);
 };
 
-d3.select('#selDataset').on('change', optionChanged(this));
-
 // Initialize the dashboard
 init();
+
+d3.select('#selDataset').on('change', optionChanged(this));
